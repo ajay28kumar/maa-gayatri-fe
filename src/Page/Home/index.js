91b1e6bd68ee7532bbed52d5fdd93051b9f35422
Ajay Kumar <ajaykumar@Ajays-MacBook-Pro.local>
2022-08-31 13:12:15 +0530

    added icon

--- src/Page/Home/index.js
+++ src/Page/Home/index.js
@@ -1,13 +1,8 @@
 import { useEffect } from 'react';
 import { connect } from 'react-redux';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import getData from '../action/getData';
+import {Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper} from '@mui/material'
+import {DeleteIcon, SaveIcon} from '@mui/icons-material'
+import getData from '../../action/getData';
 
 const Home = (props) => {
   const { data, apiState } = props || {};
